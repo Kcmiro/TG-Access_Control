f5ae587c7a9d7a9fefab9b71469ab63633663ffe
Kcmiro <rafaelfellipe1025@gmail.com>
2024-10-05 17:41:46 -0300

    Criacao da Tabela documentos

---
+++ AccessControl/models/documentos.js
@@ -0,0 +1,34 @@
+const Sequelize = require('sequelize');
+const connection = require('../database/database');
+
+const Entregas = require('../models/entregas');
+const Servico = require('../models/servicos');
+
+const Documentos = connection.define('documentos',{
+    doc_cnh:{
+        type: Sequelize.CHAR(11),
+        allowNull: false,
+        unique: true
+        
+    },
+
+    doc_cnh:{
+        type: Sequelize.CHAR(11),
+        allowNull: false,
+        unique: true
+    },
+
+    doc_empresa: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        allowNull: true
+    }
+});
+
+Documentos.belongsTo(Entregas);
+Documentos.belongsTo(Servico);
+
+
+//documentos.sync({force: true});
+
+module.exports = documentos;
