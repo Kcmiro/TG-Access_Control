6c5c0ba6f0e3bffb227c49664b49423e78fbd8a3
Kcmiro <rafaelfellipe1025@gmail.com>
2024-10-07 18:47:07 -0300

    Foi adicionados o rotas e o ativando rotas

--- AccessControl/app.js
+++ AccessControl/app.js
@@ -1,57 +1,64 @@
 //Módulos Internos
-const path = require('path');
+const path = require("path");
 
 //Módulos Externos
-const express = require('express');
-const session = require('express-session');
+const express = require("express");
+const session = require("express-session");
 
 //Modulos do Projeto
-const connection = require('./database/database');
+const connection = require("./database/database");
 
 //Criando Tabelas e colunas
-const usuario = require('./models/usuario');
-const entregas = require('./models/entregas');
-const servico = require('./models/servicos');
-const bicicletario = require('./models/bicicletario');
-const chaves = require('./models/chaves');
-const lojas = require('./models/lojas');
-const veiculos = require('./models/veiculos');
-const documentos = require('./models/documentos');
-const telefone = require('./models/telefone');
-const registro_chaves = require('./models/registro_chave');
-const registro_patio = require('./models/registro_patio');
+const usuario = require("./models/usuario");
+const entregas = require("./models/entregas");
+const servico = require("./models/servicos");
+const bicicletario = require("./models/bicicletario");
+const chaves = require("./models/chaves");
+const lojas = require("./models/lojas");
+const veiculos = require("./models/veiculos");
+const documentos = require("./models/documentos");
+const telefone = require("./models/telefone");
+const registro_chaves = require("./models/registro_chave");
+const registro_patio = require("./models/registro_patio");
+
+//Rotas
+const genericasROute = require("./routes/genericasRoute");
 
 const app = express();
 
 // Sessão
-app.use(session({
-    secret: 'Controle',
+app.use(
+  session({
+    secret: "Controle",
     cookie: {
-        maxAge: 2400000,
+      maxAge: 2400000,
     },
     resave: true,
-    saveUnitialized: false
-}));
+    saveUninitialized: true,
+  })
+);
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Banco de Dados
 connection
- .authenticate()
- .then(() => {
-    console.log('Conexão feita com Sucessso!');
- })
+  .authenticate()
+  .then(() => {
+    console.log("Conexão feita com Sucessso!");
+  })
 
- .catch(erro => { 
-     console.log(erro);
- });
- 
+  .catch((erro) => {
+    console.log(erro);
+  });
+
+//Ativando as rotas
+
+app.use("/", genericasROute);
 
 module.exports = app;
